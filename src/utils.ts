bb2fe54fec64a298d71a3b11b56b3b0d47380d4d
SATOGE1 <g.sarambeliev@gmail.com>
2026-04-26 14:44:09 +0300

    remove extra slash

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "https://marinetrustbe.onrender.com/";
+export const API_BASE_URL = "https://marinetrustbe.onrender.com";
 
 export const emptyGeoJSON = { type: "FeatureCollection", features: [] };
 
